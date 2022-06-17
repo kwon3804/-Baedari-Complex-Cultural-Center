9dc64c0d785b4d104a74674af0f29005e525f220
kwon3804 <107536563+kwon3804@users.noreply.github.com>
2022-06-17 18:52:11 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_-0608_2022-06-15-22-44-26",
+      "name": "Panorama_배다리 (06.08)_2022-06-15-22-44-26",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5253910594994196,
+          "pitch": 0.08442584874237724,
+          "rotation": 0,
+          "target": "1-panorama_-0608_2022-06-15-22-49-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4529662113442079,
+          "pitch": 0.04196944493781629,
+          "title": "배다리 도서관",
+          "text": "배다리 복합문화공간에서의 활동으로 발생한 수익금으로 운영되는 도서관"
+        },
+        {
+          "yaw": -3.1154038196376757,
+          "pitch": -0.6377522775743998,
+          "title": "양조장",
+          "text": "배다리와 시간을 함께한 전통주와의 만남"
+        },
+        {
+          "yaw": 2.67069351936356,
+          "pitch": 0.004798163165075309,
+          "title": "시음공간",
+          "text": "배다리에서 생산된 전통주를 시음할수 있는 공간"
+        },
+        {
+          "yaw": 1.5072701369748902,
+          "pitch": -0.5397920442993875,
+          "title": "다리정원",
+          "text": "윗 정원과 아랫 정원의 만남"
+        },
+        {
+          "yaw": 1.541997028494043,
+          "pitch": 0.3477088945724489,
+          "title": "소통광장",
+          "text": "소통을 위한 이웃과의 만남"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama_-0608_2022-06-15-22-49-10",
+      "name": "Panorama_배다리 (06.08)_2022-06-15-22-49-10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4328402540743568,
+          "pitch": 0.11786368064941222,
+          "rotation": 0,
+          "target": "0-panorama_-0608_2022-06-15-22-44-26"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7321304301577616,
+          "pitch": 0.11775729867015805,
+          "title": "수공간",
+          "text": "자연과의 만남"
+        },
+        {
+          "yaw": 1.50900548335572,
+          "pitch": 0.0037203555362292917,
+          "title": "카페",
+          "text": "힐링과 휴식을 위한 시간의 만남"
+        },
+        {
+          "yaw": 2.125150034895305,
+          "pitch": -0.22521163682771572,
+          "title": "갤러리",
+          "text": "과거와 현재의 배다리와의 만남"
+        }
+      ]
+    }
+  ],
+  "name": "배다리 복합문화센터",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
